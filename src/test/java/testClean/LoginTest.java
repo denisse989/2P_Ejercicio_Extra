143cddebe2ccb164227197e5533d8f2c3ee8bec0
denisse989 <denissebaldiviesopacheco@gmail.com>
2022-03-09 22:03:05 -0400

    2P Ejercicio Extra

--- src/test/java/testClean/LoginTest.java
+++ src/test/java/testClean/LoginTest.java
@@ -10,16 +10,16 @@ import page.MenuSection;
 
 public class LoginTest extends BaseTodoist {
     MainPage mainPage= new MainPage();
-    LoginPage loginModal= new LoginPage();
+    LoginPage loginPage= new LoginPage();
     MenuSection menuSection = new MenuSection();
     AccountSection accountSection=new AccountSection();
 
     @Test
     public void verifyLoginTodoist() throws InterruptedException {
         mainPage.loginImage.click();
-        loginModal.emailTxtBox.setText("deni@email.com");
-        loginModal.pwdTxtBox.setText("PassVerifi");
-        loginModal.loginButton.click();
+        loginPage.emailTxtBox.setText("deni@email.com");
+        loginPage.pwdTxtBox.setText("PassVerifi");
+        loginPage.loginButton.click();
         menuSection.profileButton.click();
         Thread.sleep(2000);
         Assertions.assertTrue(accountSection.logoutButton.isControlDisplayed(),"ERROR no se pudo hacer el login");
